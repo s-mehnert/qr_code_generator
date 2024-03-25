5256ee0ec1e54138bffad194e3067942afaa243f
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-03-25 16:37:43 +0100

    create test qr code and linkedin profile qr code, add requirements doc

---
+++ qr_code_generator.py
@@ -0,0 +1,19 @@
+import qrcode
+
+data = "test data"
+test_img = qrcode.make(data)
+test_img.save("C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/test_qrcode.png")
+
+data_2 = "test again"
+qr = qrcode.QRCode(version = 1, box_size = 8, border = 5)
+qr.add_data(data_2)
+qr.make(fit=True)
+img_2 = qr.make_image(fill_color="green", back_color="white")
+img_2.save("C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/green_qrcode.png")
+
+linkedin_link = "https://www.linkedin.com/in/smehnert/"
+qr_li = qrcode.QRCode(version=1, box_size=10, border=5)
+qr_li.add_data(linkedin_link)
+qr_li.make(fit=True)
+img_li = qr_li.make_image(fill_color="#295D8A", back_color="white")
+img_li.save("C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/linkedin_qrcode.png")
