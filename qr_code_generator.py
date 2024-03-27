fa909c84c9be2725f74d77da8eef24b8b4406158
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-03-27 19:22:57 +0100

    add functionality to automatically retrieve current working directory

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,24 +1,7 @@
 import qrcode
+import os
 
-# data = "test data"
-# test_img = qrcode.make(data)
-# test_img.save("C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/test_qrcode.png")
-
-# data_2 = "test again"
-# qr = qrcode.QRCode(version = 1, box_size = 8, border = 5)
-# qr.add_data(data_2)
-# qr.make(fit=True)
-# img_2 = qr.make_image(fill_color="green", back_color="white")
-# img_2.save("C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/green_qrcode.png")
-
-# linkedin_link = "https://www.linkedin.com/in/smehnert/"
-# qr_li = qrcode.QRCode(version=1, box_size=10, border=5)
-# qr_li.add_data(linkedin_link)
-# qr_li.make(fit=True)
-# img_li = qr_li.make_image(fill_color="#295D8A", back_color="white")
-# img_li.save("C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/linkedin_qrcode.png")
-
-# create function to build a qr code
+# function to build a qr code
 
 def create_qrcode(data, color):
     qr = qrcode.QRCode(version=1, box_size=8, border=5)
@@ -26,6 +9,8 @@ def create_qrcode(data, color):
     qr.make(fit=True)
     img = qr.make_image(fill_color=color, back_color="white")
     return img
+
+# helper function to check if file name entered is valid
 
 def name_valid(name):
     if len(name) == 0:
@@ -35,12 +20,12 @@ def name_valid(name):
             return False
     return True
 
-# create input logic
+# asking for user input
 
-print("\nWelcome to the QR Code Generator")
+print("\nWelcome to the QR Code Generator\n")
 
 data = input("\nPlease enter the text or link you would like to encode into a QR Code.\n> ")
-color = input("\nWhat color shoud the QR Code have?\nThe available options are: \nblack \nblue \nred \nbrown \ngreen \n\nNote that the background will always be white.\n> ")
+color = input("\nWhat color shoud the QR Code have?\nThe available options are: \n\n\tblack \n\tblue \n\tred \n\tbrown \n\tgreen \n\nNote that the background will always be white.\n> ")
 name = input("\nGreat, the QR Code will shortly be generated.\nUnder which name should the PNG file be saved?\n> ")
 
 while not name_valid(name):
@@ -48,13 +33,8 @@ while not name_valid(name):
     name = input("Please enter a new file name.\n> ")
 
 qrc = create_qrcode(data, color)
-location = "C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/" + name + ".png"
+location = os.getcwd() + "/" + name + ".png"
 
 qrc.save(location)
 
-print("\nThe QR Code has been successfully created. It can be found in the same directory as the QR Code Generator Python File.")
-
-# testing
-
-# qrc = create_qrcode("Hello there", "blue")
-# qrc.save("C:/Users/Peafly/Documents/Projects/Coding/projects/weekly_projects/qr_code_generator__python/new_qrcode.png")
+print("\nThe QR Code has been successfully created. It can be found in the same directory as the QR Code Generator File.")
